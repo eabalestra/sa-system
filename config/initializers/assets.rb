be6b88de0afe5f72e7739e58a394e6405c66f3dc
eabalestra <agusbalestra0@gmail.com>
2024-02-13 16:16:56 -0300

    Update application.scss and assets.rb

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,9 +10,3 @@ Rails.application.config.assets.version = "1.0"
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
-
-Rails.application.config.assets.precompile += %w( jquery.min.js jquery_ujs.js bootstrap.min.js popper.js )
-
-Rails.application.config.assets.precompile += %w( sales.css sb-admin-2.min.css transactions.css )
-
-Rails.application.config.assets.precompile += %w( cajas-login.png caja-de-entrega.ico undraw_posting_photo.svg )
